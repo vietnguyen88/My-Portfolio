622aaee0cb9b50fad5f9a890d9cc3a3365ce91e3
vietnguyen <vietnguyen.nqv@gmail.com>
2020-04-27 14:49:29 +1000

    fix misc and add animation

--- main.js
+++ main.js
@@ -7,4 +7,19 @@ let nav = selectElement('.nav');
 
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('open');
-})
+});
+
+//scroll reveal
+ScrollReveal().reveal('.animate-right', {
+    origin: 'right',
+    duration: 2000,
+    distance: '25rem',
+    delay: 600
+});
+
+ScrollReveal().reveal('.animate-left', {
+    origin: 'left',
+    duration: 2000,
+    distance: '25rem',
+    delay: 600
+});
